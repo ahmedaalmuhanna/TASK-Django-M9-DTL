164d56d06450c5d936b646a229fd92209af8ed4b
Ahmed <ahmed.a.almuhanna@hotmail.com>
2022-08-12 22:14:28 +0300

    my task

--- BareedInv/BreedProj/BreedProj/asgi.py
+++ BareedInv/BreedProj/BreedProj/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for bareed project.
+ASGI config for BreedProj project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bareed.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BreedProj.settings")
 
 application = get_asgi_application()
--- shops/views.py
+++ shops/views.py
@@ -1,3 +1,17 @@
+from multiprocessing import context
 from django.shortcuts import render
-
+# from .models import 
 # Create your views here.
+def get_ice_cream(request ,ice_cream_id ):
+    
+    context = {
+            "ice_cream": {
+                'id':'my name',
+                'name':'my name',
+                'shop':'my shop',
+                'stock':'my stock'}
+
+        
+    }
+    return render(request, 'ice_cream_details.html', context)
+    
